b58cbb6fc4d3edf397a814c2a4c4d05f46ea08ee
KeithJCross <KeithJCross@gmail.com>
2019-12-03 16:58:44 -0800

    Cat Update

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("Big Cats scarier than Big Dogs")
 	})
 	http.ListenAndServe(":8080", nil)
 }
